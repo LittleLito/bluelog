ad7aac1dba52b5a06bfd613d2a3a37eb0fa594d4
Lito <Lizehong519@outlook.com>
2020-07-29 16:07:58 +0800

    models estabished, fake functions created
    finished models and the fake-data-create functions
    the procedure can work correctly

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import os
+import click
 from bluelog.settings import config
 from bluelog.extensions import bootstrap, db, moment, ckeditor, mail
 from bluelog.blueprints.blog import blog_bp
@@ -54,10 +55,33 @@ def register_template_context(app):
 
 
 def register_errors(app):
-    @app.errorhandle(400)
+    @app.errorhandler(400)
     def bad_request(e):
         return render_template('errors/400.html'), 400
 
 
 def register_commands(app):
-    pass
+    @app.cli.command()
+    @click.option('--category', default=10, help='Quantity of categories, default is 10.')
+    @click.option('--post', default=50, help='Quantity of posts, default is 50.')
+    @click.option('--comment', default=500, help='Quantity of comments, default is 500.')
+    def forge(category, post, comment):
+        """Generates the fake categories, posts and comments."""
+        from bluelog.fakes import fake_admin, fake_categories, fake_posts, fake_comments
+
+        db.drop_all()
+        db.create_all()
+
+        click.echo('Generating the administrator...')
+        fake_admin()
+
+        click.echo(f'Generating {category} categories...')
+        fake_categories(category)
+
+        click.echo(f'Generating {post} posts...')
+        fake_posts(post)
+
+        click.echo(f'Generating {comment} comments...')
+        fake_comments(comment)
+
+        click.echo('Done')
